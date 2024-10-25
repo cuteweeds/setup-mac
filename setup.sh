9257f17cee66d0769e103da45667ec63a531a395
cuteweeds <105590544+cuteweeds@users.noreply.github.com>
2024-10-25 19:59:55 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,19 @@
 # Check for git, exit if it's missing
 if test ! $(which git); then
-  echo "Git not found. Install it whatever way is best and re-run this script."
+  echo -e "\nGit not found. Install it whatever way is best and re-run this script."
   exit
 fi
 
 # Check for xcode-select,
 # Install if we don't have it
 if test ! $(which xcode-select); then
-  echo "Installing xcode-stuff"
+  echo -e "\nInstalling xcode-stuff"
   xcode-select --install
 fi
 
 # Install homebrew if we don't have it
 if test ! $(which brew); then
-  echo "Installing homebrew..."
+  echo -e "\nInstalling homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
@@ -26,17 +26,19 @@ brew install gh
 
 brew cleanup
 
-echo "Log into git"
+
+echo -e "\nLog into git"
 gh auth login
 
-echo -e "\nSetting up dotfiles.."
-cd ~/
 
-echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.zshrc
-echo "alias dot='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.zshrc
-echo ".dotfiles" >> .gitignore
+echo -e "\nSetting up dotfiles.."
+cd $HOME
 
 git clone --bare https://github.com/cuteweeds/.dotfiles $HOME/.dotfiles
+
+echo ".dotfiles" >> .gitignore
+echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.zshrc
+echo "alias dot='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'" >> $HOME/.zshrc
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 echo -e "\n...backing up existing config files"
@@ -49,16 +51,18 @@ dotfiles config --local status.showUntrackedFiles no
 dot status
 
 echo -e "\nIf you see \"On branch <name>\" above, dotfiles installed correctly."
-echo -e "\n\033[1m\033[32mImportant commands"
-echo -e "\033[0m\033[32mstatus\t.dotfiles repo status"
-echo -e "dot add\tadd file (from ~/)"
-echo -e "dot commit, dot push etc.\033[0m"
+echo -e "\n\033[32m\033[1mUpdating dotfiles"
+echo -e  "\n\033[1m\033[36mUse 'dot' instead of 'git' for dotfile maintenance\033[0m\033[36m\n- dot status\n- dot add <file>\n- dot push\n- dotcetera...\033[0m"
 
 echo "Press any key to continue..."
 read -n 1 -s
 
+
 echo -e "\n\033[1m\033[36mPackage Install list\033[0m"
 cat Brewfile
+
+echo "Press any key to continue..."
+read -n 1 -s
 
 brew bundle install
 brew cleanup
