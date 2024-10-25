a7a10590897ec0605c6d7e40ca44fa09019e3c76
cuteweeds <105590544+cuteweeds@users.noreply.github.com>
2024-10-25 16:59:10 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,6 +16,7 @@ if test ! $(which brew); then
   echo "Installing homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+
 brew update
 brew upgrade
 
@@ -36,19 +37,13 @@ echo -e "\n...backing up existing config files"
 mkdir -p $HOME/.config-backup && \
 dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
 xargs -I{} mv {} $HOME/.config-backup/{}
-#not sure this is needed 
-#dotfiles checkout
+dotfiles checkout
 
 dotfiles config --local status.showUntrackedFiles no
-
 dot status
+
 echo -e "\nIf you see \"On branch <name>\" above, dotfiles installed correctly."
 echo -e "\n\033[1m\033[32mImportant commands"
 echo -e "\033[0m\033[32mstatus\t.dotfiles repo status"
 echo -e "dot add\tadd file (from ~/)"
 echo -e "dot commit, dot push etc.\033[0m"
-
-
-#git clone https://github.com/cuteweeds/.dotfiles ~/.dotfiles
-#cd .dotfiles
-#./install-dotfiles.sh
