29ae73bc54d15e3164d0da83f4ac3fe33449892a
cuteweeds <105590544+cuteweeds@users.noreply.github.com>
2024-10-25 13:52:45 -0400

    Delete mac-master.sh

--- mac-master.sh
+++
@@ -1,7 +0,0 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew update
-brew upgrade
-brew install --cask git-credential-manager
-git clone https://github.com/cuteweeds/.dotfiles
-cd .dotfiles
-./install-dotfiles.sh
