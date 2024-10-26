9b483546bc672bca60e9257eeabffea925adc423
cuteweeds <105590544+cuteweeds@users.noreply.github.com>
2024-10-25 20:30:27 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -44,7 +44,7 @@ alias dot='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 echo -e "\n...backing up existing config files"
 mkdir -p $HOME/.config-backup && \
-/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
 xargs -I{} mv {} $HOME/.config-backup/{}
 /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout checkout
 
