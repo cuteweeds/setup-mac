76d68b999fd0a67b61e7f26cd19b31f0ce022694
cuteweeds <105590544+cuteweeds@users.noreply.github.com>
2024-10-25 23:15:36 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+echo -e "\033[34mcw setup script from \n  https://raw.githubusercontent.com/cuteweeds/setup-mac/HEAD/setup.sh"
 ## Check for git, exit if it's missing
 #if test ! $(which git); then
 #  echo -e "\nGit not found. Install it whatever way is best and re-run this script."
@@ -31,15 +32,15 @@
 #gh auth login
 
 
-echo -e "\nSetting up dotfiles.."
+echo -e "\033[35m\nSetting up dotfiles.."
 cd $HOME
 git clone --bare https://github.com/cuteweeds/.dotfiles $HOME/.dotfiles
 
-echo -e "\nwriting to .gitignore"
+echo -e "\033[36m\nwriting to .gitignore"
 echo ".dotfiles" >> .gitignore
 
 echo "backing up existing config files"
-mkdir -p $HOME/.config-backup && git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout 2>&1 | egrep "\s+[A-Za-z0-9\._-]" | grep -v "^error:" | grep -v "^Please move or remove them before you switch branches" | awk {'print $1'} | xargs -I{} mv {} $HOME/.config-backup/{}
+mkdir -p $HOME/.config-backup && git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout 2>&1 | egrep "\s+.+:\s+." | grep -v "^error:" | grep -v "^Please move or remove them before you switch branches" | awk {'print $1'} | xargs -I{} mv {} $HOME/.config-backup/{}
 
 echo "Press any key to continue..."
 read -n 1 -s
