94c59587ddd09f8e530c0cfceee50ce30de45ca3
cuteweeds <105590544+cuteweeds@users.noreply.github.com>
2024-10-25 22:41:07 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -41,7 +41,6 @@ echo ".dotfiles" >> .gitignore
 echo "backing up existing config files"
 mkdir -p $HOME/.config-backup && git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout 2>&1 | egrep "\s+[A-Za-z0-9\._-]" | grep -v "^error:" | grep -v "^Please move or remove them before you switch branches" | awk {'print $1'} | xargs -I{} mv {} $HOME/.config-backup/{}
 
-
 echo "Press any key to continue..."
 read -n 1 -s
 
@@ -65,14 +64,12 @@ alias dot="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 echo -e "\nIf you see \"On branch <name>\" above, dotfiles installed correctly."
 echo -e "\n\033[32m\033[1mUpdating dotfiles"
 echo -e  "\n\033[1m\033[36mUse 'dot' instead of 'git' for dotfile maintenance\033[0m\033[36m\n- dot status\n- dot add <file>\n- dot push\n- dotcetera...\033[0m\n"
-
+exit
 echo "Press any key to continue..."
 read -n 1 -s
 
 echo -e "\n\033[1m\033[36mPackage Install list\033[0m\n"
 cat Brewfile
-
-exit
 
 echo "Press any key to continue..."
 read -n 1 -s
